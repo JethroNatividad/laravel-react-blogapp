08ab361c6f7d7d956085b77adba421e35f5232a9
JethroNatividad <natividad.jet@gmail.com>
2024-10-31 01:31:40 +0800

    feat: add navlink

--- resources/js/Layouts/Layout.tsx
+++ resources/js/Layouts/Layout.tsx
@@ -3,6 +3,7 @@ import { Link, usePage } from "@inertiajs/react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/Components/Avatar";
 import { Popover, PopoverContent, PopoverTrigger } from "@/Components/Popover";
 import { Button } from "@/Components/Button";
+import NavLink from "@/Components/NavLink";
 
 const Layout = ({ children }: PropsWithChildren) => {
     const user = usePage().props.auth.user;
@@ -10,8 +11,20 @@ const Layout = ({ children }: PropsWithChildren) => {
     return (
         <div>
             <header className="shadow-sm">
-                <div className="container mx-auto py-6 flex justify-between">
-                    <h1 className="text-2xl font-medium">Another Blog</h1>
+                <div className="container mx-auto py-6 flex justify-between items-center">
+                    <div className="flex items-center space-x-6">
+                        <h1 className="text-2xl font-medium">
+                            Another Blog Site
+                        </h1>
+                        <nav>
+                            <NavLink
+                                href={route("home")}
+                                active={route().current("home")}
+                            >
+                                Home
+                            </NavLink>
+                        </nav>
+                    </div>
                     <Popover>
                         <PopoverTrigger>
                             <Avatar>
